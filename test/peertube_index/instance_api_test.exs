8bc3a0d24b90166623d71fa580a12bba2fd5591c
silicium14 <13800015+silicium14@users.noreply.github.com>
2019-01-06 13:59:01 +0100

    Improve instance API tests

--- test/peertube_index/instance_api_test.exs
+++ test/peertube_index/instance_api_test.exs
@@ -67,25 +67,62 @@ defmodule PeertubeIndex.InstanceAPITest do
     }}
   end
 
-  test "bad HTTP status", %{bypass: bypass} do
-    Bypass.expect_once bypass, "GET", "/api/v1/videos", fn conn ->
-      Plug.Conn.resp(conn, 400, "{}")
+  defp empty_instance() do
+    %{
+      {"GET", "/api/v1/videos"} => {
+        :stub, fn conn ->
+          Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
+        end},
+      {"GET", "/api/v1/server/followers"} => {
+          :stub, fn conn ->
+            Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
+          end},
+      {"GET", "/api/v1/server/following"} => {
+          :stub, fn conn ->
+            Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
+          end}
+    }
+  end
+
+  defp overwrite_expectation(routes, type, method, path, function) do
+    Map.put(routes, {method, path}, {type, function})
+  end
+
+  defp apply_bypass(routes, bypass) do
+    for {{method, path}, {expect_function, function}} <- routes do
+      case expect_function do
+        :stub ->
+          Bypass.stub(bypass, method, path, function)
+        :expect ->
+          Bypass.expect(bypass, method, path, function)
+        :expect_once ->
+          Bypass.expect_once(bypass, method, path, function)
+        invalid ->
+          raise "Invalid expect function #{invalid}"
+      end
     end
+  end
+
+  defp empty_instance_but(bypass, expect_function, method, path, function) do
+    empty_instance()
+    |> overwrite_expectation(expect_function, method, path, function)
+    |> apply_bypass(bypass)
+  end
+
+  test "bad HTTP status", %{bypass: bypass} do
+    empty_instance_but(bypass, :expect_once, "GET", "/api/v1/videos", &Plug.Conn.resp(&1, 400, "{}"))
     result = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 100, false)
     assert result == {:error, :http_error}
   end
 
   test "JSON parse error", %{bypass: bypass} do
-    Bypass.expect_once bypass, "GET", "/api/v1/videos", fn conn ->
-      Plug.Conn.resp(conn, 200, "invalid JSON document")
-    end
-
+    empty_instance_but(bypass, :expect_once, "GET", "/api/v1/videos", &Plug.Conn.resp(&1, 200, "invalid JSON document"))
     result = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 100, false)
     assert result == {:error, %Poison.ParseError{pos: 0, rest: nil, value: "i"}}
   end
 
-  test "error after fist page", %{bypass: bypass}do
-    Bypass.expect bypass, "GET", "/api/v1/videos", fn conn ->
+  test "error after fist page", %{bypass: bypass} do
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/videos", fn conn ->
       conn = Plug.Conn.fetch_query_params(conn)
       response = if Map.has_key?(conn.query_params, "start") do
         ~s<bad json>
@@ -93,99 +130,61 @@ defmodule PeertubeIndex.InstanceAPITest do
         ~s<{"total": 10, "data": []}>
       end
       Plug.Conn.resp(conn, 200, response)
-    end
+    end)
 
     {status, _} = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 5, false)
     assert status == :error
   end
 
   test "error on followers", %{bypass: bypass} do
-    Bypass.stub bypass, "GET", "/api/v1/videos", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/followers", fn conn ->
-      Plug.Conn.resp(conn, 500, "Error")
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/following", fn conn ->
-      Plug.Conn.resp(conn, 200, "Error")
-    end
-
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/server/followers", &Plug.Conn.resp(&1, 500, "Error"))
     result = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 100, false)
-
     assert result == {:error, :http_error}
   end
 
   test "error on following", %{bypass: bypass} do
-    Bypass.stub bypass, "GET", "/api/v1/videos", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/followers", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/following", fn conn ->
-      Plug.Conn.resp(conn, 500, "Error")
-    end
-
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/server/following", &Plug.Conn.resp(&1, 500, "Error"))
     result = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 100, false)
-
     assert result == {:error, :http_error}
   end
 
   test "gets all videos correctly with a single page", %{bypass: bypass} do
-    Bypass.expect bypass, "GET", "/api/v1/videos", fn conn ->
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/videos", fn conn ->
       conn = Plug.Conn.fetch_query_params(conn)
-      Plug.Conn.resp(conn, 200, ~s<{"total": 2, "data": [{"id": 0, "isLocal": true}, {"id": 1, "isLocal": true}]}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/followers", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/following", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
+      Plug.Conn.resp(conn, 200, ~s<{
+        "total": 2,
+        "data": [
+          {"id": 0, "isLocal": true},
+          {"id": 1, "isLocal": true}
+        ]
+      }>)
+    end)
 
     {:ok, {videos, _instances}} = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 10, false)
     assert videos == [
-             %{"id" =>  0, "isLocal" => true},
-             %{"id" =>  1, "isLocal" => true}
-           ]
+      %{"id" =>  0, "isLocal" => true},
+      %{"id" =>  1, "isLocal" => true}
+    ]
   end
 
   test "gets all videos correctly with pagination", %{bypass: bypass} do
-    Bypass.expect bypass, "GET", "/api/v1/videos", fn conn ->
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/videos", fn conn ->
       conn = Plug.Conn.fetch_query_params(conn)
       start = Map.get(conn.query_params, "start", "0")
       Plug.Conn.resp(conn, 200, ~s<{"total": 3, "data": [{"id": #{start}, "isLocal": true}]}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/followers", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
-
-    Bypass.stub bypass, "GET", "/api/v1/server/following", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<{"total": 0, "data": []}>)
-    end
+    end)
 
     {:ok, {videos, _instances}} = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 1, false)
     assert videos == [
-             %{"id" =>  0, "isLocal" => true},
-             %{"id" =>  1, "isLocal" => true},
-             %{"id" =>  2, "isLocal" => true}
-           ]
+      %{"id" =>  0, "isLocal" => true},
+      %{"id" =>  1, "isLocal" => true},
+      %{"id" =>  2, "isLocal" => true}
+    ]
   end
 
   test "wrong page format", %{bypass: bypass} do
-    Bypass.expect_once bypass, "GET", "/api/v1/videos", fn conn ->
-      Plug.Conn.resp(conn, 200, "{\"not the correct format\": \"some value\"}")
-    end
-
+    empty_instance_but(bypass, :expect, "GET", "/api/v1/videos", &Plug.Conn.resp(&1, 200, "{\"not the correct format\": \"some value\"}"))
     result = PeertubeIndex.InstanceAPI.Httpc.scan("localhost:#{bypass.port}", 100, false)
     assert result == {:error, :page_invalid}
   end
-
 end
