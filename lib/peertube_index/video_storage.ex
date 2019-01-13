b4e2c8662047694e29d761ae6b5c72acb9b7a555
silicium14 <13800015+silicium14@users.noreply.github.com>
2019-01-13 09:44:44 +0100

    Move VideoStorage implementation in the same file as the behaviour, replace video_storage directory with a single file

--- lib/peertube_index/video_storage.ex
+++ lib/peertube_index/video_storage.ex
@@ -1,3 +1,33 @@
+defmodule PeertubeIndex.VideoStorage do
+  @moduledoc false
+
+  @doc """
+  Updates an instance with a list of videos.
+  This removes existing videos for the given instance and inserts the given videos.
+  """
+  @callback update_instance!(String.t, [map]) :: :ok
+
+  @doc """
+  Search for a video by its name
+  Options:
+    - nsfw:
+      - missing: do not filter on safety, gets both safe and unsafe videos
+      - true: only get unsafe for work videos
+      - false: only get safe for work videos
+  """
+  @callback search(String.t, Keyword.t()) :: [map]
+
+  @doc """
+  Create a video storage with some videos for testing
+  """
+  @callback with_videos([map]) :: :ok
+
+  @doc """
+  Create an empty video storage for testing
+  """
+  @callback empty() :: :ok
+end
+
 defmodule PeertubeIndex.VideoStorage.Elasticsearch do
   @moduledoc false
 
