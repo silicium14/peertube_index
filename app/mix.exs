7c5d6dac803c2974e173a8a086f3554548010653
silicium14 <13800015+silicium14@users.noreply.github.com>
2019-06-17 23:04:32 +0200

    Bump elixir dependencies versions

--- app/mix.exs
+++ app/mix.exs
@@ -25,18 +25,18 @@ defmodule PeertubeIndex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:confex, "~> 3.3"},
-      {:elasticsearch, "~> 0.6.0"},
+      {:confex, "~> 3.4"},
+      {:elasticsearch, "~> 1.0"},
       {:gollum, github: "silicium14/gollum", ref: "ff84c9c00433ce0d5ff75697ec2f32d34750d6d8"},
       {:phoenix_html, "~> 2.13"},
-      {:plug, "~> 1.7"},
+      {:plug, "~> 1.8"},
       {:plug_cowboy, "~> 2.0"},
       {:poison, "~> 4.0"},
       # Dev only
       {:bypass, "~> 1.0.0", only: :test},
-      {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:credo, "~> 1.1", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
-      {:mox, "~> 0.4.0", only: :test},
+      {:mox, "~> 0.5.1", only: :test},
       {:remix, "~> 0.0.2", only: :dev}
     ]
   end
