df58b1a1effee3b6c48ce533ea9685e34e6b9ef3
silicium14 <13800015+silicium14@users.noreply.github.com>
2019-09-08 17:43:00 +0200

    Remove infrastructure code and put elixir app at the root of the repository

--- status_monitoring_updater/create_database.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-psql \
-    -h status-monitoring-db \
-    -U postgres \
-    -v source_host="'$SOURCE_HOST'" \
-    -v source_user="'$SOURCE_USER'" \
-    -v source_database="'$SOURCE_DATABASE'" \
-    -f create_database.sql
